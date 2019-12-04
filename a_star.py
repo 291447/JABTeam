f67d28d90da26e84cdaa51c4675c0e44da4eb1ee
x291448 <56122596+x291448@users.noreply.github.com>
2019-12-04 10:23:47 +0100

    Add files via upload

--- a_star.py
+++ a_star.py
@@ -5,65 +5,75 @@ from heuristic import *
 
 def A_star(graf, start, end):
 	visited = set()
-	visited.add(start)
+	visited.add(start.id)
+	
 	cameFrom = {}                   #wierzcholki odwiedzone
 	cameFrom[start.id] = None
-	added_to_not_visited = set()
 	
-	not_visited = FibonacciHeap()
+	fibHeap = FibonacciHeap()
+	added_to_fibHeap = set()
+	
 	g_score = {}                              #odleglosc od startu
 	g_score[start.id] = 0
-	nd = start
+	
+	prevNode = start
 	i = 0
 
-	while (nd.id != end.id):
+	while (prevNode.id != end.id):
 		i = i + 1
-		for y in nd.edges:
-			if y.toNode.id == nd.id:
-				n = Node()
-				n = y.toNode
-				y.toNode = y.fromNode
-				y.fromNode = n
-			if y.toNode.id in visited:
+		
+		for nextEdge in prevNode.edges:
+		
+			if nextEdge.toNode.id == prevNode.id:
+				nextEdge.toNode, nextEdge.fromNode = nextEdge.fromNode, nextEdge.toNode
+			
+			if nextEdge.toNode.id in visited:
 				continue
-			if y.toNode.id not in g_score:
-				g_score[y.toNode.id] = g_score[nd.id] + y.length
-				cameFrom[y.toNode.id] = nd.id
+			
+			if nextEdge.toNode.id not in g_score:
+				g_score[nextEdge.toNode.id] = g_score[prevNode.id] + nextEdge.length
+				cameFrom[nextEdge.toNode.id] = prevNode.id
 			else:
-				prev_g = g_score[y.toNode.id]
-				cur_g = g_score[nd.id] + y.length
+				prev_g = g_score[nextEdge.toNode.id]
+				cur_g = g_score[prevNode.id] + nextEdge.length
 				if cur_g < prev_g:
-					g_score[y.toNode.id] = cur_g
-					cameFrom[y.toNode.id] = nd.id
-			y.toNode.h = heuristic_euc(y.toNode.x, y.toNode.y, end.x, end.y)
-			y.toNode.f = g_score[y.toNode.id] + y.toNode.h
-			y_key = [y.toNode.f, y.toNode.id]
-			#not_visited.reorder()
-			if y_key[1] not in added_to_not_visited:
-				not_visited.insert(y_key)
-				added_to_not_visited.add(y_key[1])
+					g_score[nextEdge.toNode.id] = cur_g
+					cameFrom[nextEdge.toNode.id] = prevNode.id
+			
+			h = heuristic_euc(nextEdge.toNode.x, nextEdge.toNode.y, end.x, end.y)
+			f = g_score[nextEdge.toNode.id] + h
+			
+			y_key = (f, nextEdge.toNode.id)
+			if y_key[1] not in added_to_fibHeap:
+				fibHeap.insert(y_key)
+				added_to_fibHeap.add(y_key[1])
 			else:
-				minim = not_visited.get_min()
+				minim = fibHeap.get_min()
 				if minim[1] == y_key[1]:
 					if minim[0] > y_key[0]:
-						not_visited.extract_min()
-						not_visited.insert(y_key)
+						fibHeap.extract_min()
+						fibHeap.insert(y_key)
 				else:
-					not_visited.insert(y_key)
-					added_to_not_visited.add(y_key[1])
-		x = not_visited.extract_min()
-		if x[1] in added_to_not_visited:
-			added_to_not_visited.remove(x[1])
-		nd = graf.node[x[1]]
-		visited.add(nd.id)
-	l = nd.id
+					fibHeap.insert(y_key)
+					
+		nodeid = fibHeap.extract_min()[1]
+		while (nodeid in visited):
+			nodeid = fibHeap.extract_min()[1]
+			if nodeid in added_to_fibHeap:
+				added_to_fibHeap.remove(nodeid)
+
+		prevNode = graf.node[nodeid]
+		visited.add(prevNode.id)
+
+	l = prevNode.id
 	path = []
+	
 	while cameFrom[l] != None:
-		prevNode = cameFrom[l]
-		#final_cost = final_cost + prevNode.final_cost
-		#print(final_cost)
-		path.append(prevNode)
-		l = prevNode
+		nd = cameFrom[l]
+		path.append(nd)
+		l = nd
+
 	path.reverse()
-	path.append(nd.id)
-	print(path, "liczba iter: ", i, "dlugosc sciezki: ", len(path))
+	path.append(prevNode.id)
+	
+	return (path, i)
